c61b4217bea0564c511f29664b599c571460846f
Maiquel <junior.mafra13062005@gmail.com>
2026-01-27 18:32:22 -0300

    Limpeza de Arquivos

--- Asp.Net_Projeto01_HellowAsp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Asp.Net_Projeto01_HellowAsp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
